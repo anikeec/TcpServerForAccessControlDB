d62a7637231a84899ba6949e6d261a27c0e0a3a8
apu <pasha_anik@ukr.net>
2018-09-20 18:44:19 +0300

    Add method to InfoMessageRepository

--- src/main/java/com/apu/TcpServerForAccessControlDB/repository/InfoMessageRepository.java
+++ src/main/java/com/apu/TcpServerForAccessControlDB/repository/InfoMessageRepository.java
@@ -6,6 +6,9 @@
 package com.apu.TcpServerForAccessControlDB.repository;
 
 import com.apu.TcpServerForAccessControlDB.entity.InfoMessage;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -13,5 +16,7 @@ import org.springframework.data.repository.CrudRepository;
  * @author apu
  */
 public interface InfoMessageRepository extends CrudRepository<InfoMessage, Integer>{
+    
+    public List<InfoMessage> findAll();
     
 }
