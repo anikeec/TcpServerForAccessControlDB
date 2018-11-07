007118c55d53a3dde3e80e46824349a82af27f50
apu <pasha_anik@ukr.net>
2018-11-07 13:43:05 +0200

    Try fix error duplicate key value violates unique constraint

--- src/main/java/com/apu/TcpServerForAccessControlDB/entity/SystemUser.java
+++ src/main/java/com/apu/TcpServerForAccessControlDB/entity/SystemUser.java
@@ -55,7 +55,7 @@ public class SystemUser implements Serializable, ActivatableEntity {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Basic(optional = false)
     @Column(name = "user_id", nullable = false, unique = true)
     private Integer userId;
