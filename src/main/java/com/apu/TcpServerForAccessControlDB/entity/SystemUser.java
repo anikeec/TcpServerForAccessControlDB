4e6e057c9ce36949af4c109146510f1108cf286e
apu <pasha_anik@ukr.net>
2019-01-12 12:35:57 +0200

    Refactoring

--- src/main/java/com/apu/TcpServerForAccessControlDB/entity/SystemUser.java
+++ src/main/java/com/apu/TcpServerForAccessControlDB/entity/SystemUser.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.apu.TcpServerForAccessControlDB.entity;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Basic;
@@ -18,14 +14,11 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-//import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
@@ -33,6 +26,10 @@ import javax.xml.bind.annotation.XmlTransient;
 
 import com.apu.TcpServerForAccessControlDB.interfaces.ActivatableEntity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  *
@@ -43,6 +40,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @XmlRootElement
 //@RedisHash("systemUser")
 @JsonIgnoreProperties("cardCollection")
+@RequiredArgsConstructor
 @NamedQueries({
     @NamedQuery(name = "SystemUser.findAll", query = "SELECT u FROM SystemUser u")
     , @NamedQuery(name = "SystemUser.findByUserId", query = "SELECT u FROM SystemUser u WHERE u.userId = :userId")
@@ -54,102 +52,53 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class SystemUser implements Serializable, ActivatableEntity {
 
     private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "user_id", nullable = false, unique = true)
+    @Getter @Setter
     private Integer userId;
-//    @Size(max = 255)
-    @Column(name = "first_name", length = 255)
-    private String firstName;
-//    @Size(max = 255)
-    @Column(name = "second_name", length = 255)
-    private String secondName;
-//    @Size(max = 255)
-    @Column(name = "phone_number", length = 255)
-    private String phoneNumber;
-    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
-//    @Size(max = 255)
-    @Column(name = "email", length = 255)
-    private String email;
-    @Column(name = "password", length = 255)
-    private String password;
-    @Column(name = "active")
-    private Boolean active = false;
-    @OneToMany(mappedBy = "userId", fetch = FetchType.LAZY)
-    private Collection<Card> cardCollection;
-    @ManyToMany(mappedBy = "userCollection")
-    private Set<UserRole> userRoleCollection;
-
-    public SystemUser() {
-    }
-
-    public SystemUser(Integer userId) {
-        this.userId = userId;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
     
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
+    @Column(name = "first_name", length = 255)
+    @Getter @Setter
+    private String firstName;
+    
+    @Column(name = "second_name", length = 255)
+    @Getter @Setter
+    private String secondName;
+    
+    @Column(name = "phone_number", length = 255)
+    @Getter @Setter
+    private String phoneNumber;
+    
+    @Column(name = "email", length = 255)
+    @Getter @Setter
+    private String email;
+    
+    @Column(name = "password", length = 255)
+    @Getter @Setter
+    private String password;
+    
+    @Column(name = "active")
+    @Getter @Setter
+    private Boolean active = false;
+    
+    @OneToMany(mappedBy = "userId", fetch = FetchType.LAZY, 
+                cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.REMOVE})
+    private List<Card> cardCollection = new ArrayList<>();
+    
+    @ManyToMany(mappedBy = "userCollection")
+    private Set<UserRole> userRoleCollection = new HashSet<>();
+    
+    
 
     @XmlTransient
-    public Collection<Card> getCardCollection() {
+    public List<Card> getCardCollection() {
         return cardCollection;
     }
 
-    public void setCardCollection(Collection<Card> cardCollection) {
+    public void setCardCollection(List<Card> cardCollection) {
         this.cardCollection = cardCollection;
     }
     
